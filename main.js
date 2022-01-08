139d71faada83e019a3de039bdca4a548abbe499
Michel Wene <michelwene@hotmail.com>
2022-01-08 18:34:34 -0300

    last adjust html, css and JS

---
+++ main.js
@@ -0,0 +1,6 @@
+const arrow = document.querySelector(".seta");
+const show = document.querySelector(".compartilhar");
+
+arrow.addEventListener("click", function () {
+  show.classList.toggle("show");
+});
